6842fdc12cdf5377dc7c9185fba31007973e3d5a
Evgeniy <freemen0083@gmail.com>
2023-04-15 20:48:37 +0400

    1

---
+++ 1.py
@@ -0,0 +1,13 @@
+import random
+n=int(input('input length: '))
+l=[]
+i=0
+count=0
+for i in range(n):
+    l.append(random.randint(1,5))
+    print(l[i],end='  ')
+f=int(input('\ninput number : '))
+for i in range(n):
+    if f == l[i]:
+        count+=1
+print(count)
